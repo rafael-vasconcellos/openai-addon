dbcefb41c04d5a1ee15a1e30368c5294c4d6ea11
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-11-14 23:41:17 +0000

    update dead imports

--- src/Engine/custom.ts
+++ src/Engine/custom.ts
@@ -1,31 +1,3 @@
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, GoogleGenerativeAIResponseError, PromptFeedback } from "@google/generative-ai";
-import { systemPrompt, userPrompt } from "./Prompt";
-
-
-interface IGoogleFilterBlock { 
-    text: CallableFunction // throws the error
-    functionCall: CallableFunction
-    functionCalls: CallableFunction
-    usageMetadata: Record<string, unknown>
-    promptFeedback: PromptFeedback
-}
-
-const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    }, {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    }, {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    }, {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    }
-]
-
 export class CustomEngine extends TranslatorEngine { 
     public api_key?: string
     public target_language: string = "English - US"
